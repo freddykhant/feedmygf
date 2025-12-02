bcf0ed9c60a1ca4db89a0dbafb743ab6fcdda7e8
Freddy <freddyhmk@gmail.com>
2025-12-03 01:42:03 +0800

    remove eat route

--- src/app/eat/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function EatPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-linear-to-b from-pink-50 via-orange-50 to-blue-100">
-      <h1 className="text-4xl font-bold text-gray-900">Welcome to Eat Page</h1>
-    </div>
-  );
-}
